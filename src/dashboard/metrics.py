5d313c4fff46124d05295b405594d35e0c0e5a3c
sumeethande <hande.sumeet38@gmail.com>
2026-04-23 00:14:54 +0200

    Added OverviewKPI dataclass
    Defined and added OverviewKPI dataclass in metrics module for KPI's of the Overview dashboard.

--- src/dashboard/metrics.py
+++ src/dashboard/metrics.py
@@ -1,2 +1,18 @@
 """This module contains dataclasses defining dashboard data and how it is stored.
 """
+
+from dataclasses import dataclass, field
+
+@dataclass
+class OverviewKPI:
+    """Represents a KPI's data of the Overview Dashboard.
+
+    Attributes:
+        current (float): Total value of the current period.
+        previous (float): Total value of the previous period.
+        delta (float): Difference between values of current & previous period.
+    """
+
+    current: float | None = None
+    previous: float | None = None
+    delta: float | None = None
